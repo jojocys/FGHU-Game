3d8d99ece91952aecfc1ca6dcddbe8f160caf7a7
ObservedObserver <yuyilobay@gmail.com>
2023-02-12 21:03:54 +0800

    feat: transform

--- base.py
+++ base.py
@@ -11,13 +11,22 @@ def get_starred_users(repo_name, access_token):
         response = requests.get(url, headers=headers)
         data = response.json()
         users.extend(data)
+        # if len(users) > 3200:
+        #     break
         if "next" in response.links:
             url = response.links["next"]["url"]
         else:
             break
     return users
 
-starred_users = get_starred_users("<repo-name>", "<access-token>")
+# starred_users = get_starred_users("<repo-name>", "<access-token>")
 
-with open("starred_users.json", "w") as file:
-    json.dump(starred_users, file)
+with open("./.tokens.json", "r") as file1:
+    tokens = json.load(file1)
+    github_access_token = tokens["github_access_token"]
+    print(github_access_token)
+    repo_name = "kataras/iris"
+    save_file_name = repo_name.replace("/", "_")
+    with open(f"./data/starred_users_{save_file_name}.json", "w") as file2:
+        starred_users = get_starred_users(repo_name, github_access_token)
+        json.dump(starred_users, file2)
